5b3d205d59f27a3251d974a2f2e5cbabff044ea5
RmStorm <roaldstorm@gmail.com>
2019-09-30 14:21:42 +0200

    Added a timeout to wait for the short-lived container to shut down
    Also updated requirements.txt to fix #36 and #37 and updated the version to 3.1.2

--- tests/pytest_docker_compose_tests/test_module_scoping_fixtures.py
+++ tests/pytest_docker_compose_tests/test_module_scoping_fixtures.py
@@ -1,8 +1,10 @@
-import pytest
+import time
 import requests
 from urllib.parse import urljoin
 from urllib3.util.retry import Retry
 from requests.adapters import HTTPAdapter
+
+import pytest
 
 pytest_plugins = ["docker_compose"]
 
@@ -19,8 +21,15 @@ def wait_for_api(module_scoped_container_getter):
     service = module_scoped_container_getter.get("my_api_service").network_info[0]
     api_url = "http://%s:%s/" % (service.hostname, service.host_port)
     assert request_session.get(api_url)
-    # The container that only echoes should have run and disappeared by now:
-    assert not module_scoped_container_getter.get("my_service_without_containers")
+
+    start = time.time()
+    while module_scoped_container_getter.get("my_short_lived_service"):
+        if time.time() - start >= 5:
+            raise RuntimeError(
+                'my_short_lived_service should spin up, echo "Echoing" and '
+                'then shut down, since it still running something went wrong'
+            )
+        time.sleep(.5)
     return request_session, api_url
 
 
